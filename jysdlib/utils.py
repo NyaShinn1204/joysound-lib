f760899f29d3a34be660ad432d62f2a94a4f6b6f
NyaShinn1204 <team.nyarara@gmail.com>
2024-03-21 21:53:47 +0900

    Add get_lyric
    :sparkles: 歌詞をsongidから取得するものを追加しました

--- jysdlib/utils.py
+++ jysdlib/utils.py
@@ -2,7 +2,7 @@ import requests
 import json
 import re
 
-def set_data(name: str = None):
+def set_data(name: str = None, songid: int = None):
     payload1 = {
         "kind1": "compound",
         "word1": name,
@@ -13,6 +13,12 @@ def set_data(name: str = None):
         "count": "5",
         "sort": "popular",
         "order": "desc",
+        "apiVer": "1.0",
+    }
+    payload2 = {
+        "kind": "naviGroupId",
+        "selSongNo": songid,
+        "interactionFlg": "0",
         "apiVer": "1.0",
     }
     header = {
@@ -32,7 +38,7 @@ def set_data(name: str = None):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
         "X-Jsp-App-Name": "0000800"
     }
-    return header, payload1
+    return header, payload1, payload2
 
 def search_song(song_name: str, artist: str = None, show_count: int = None, all_hit: bool = False, include_sabikara: bool = False):
     """
@@ -40,13 +46,15 @@ def search_song(song_name: str, artist: str = None, show_count: int = None, all_
     曲を検索します
 
     ※ デフォルトの表示数は5です
+    
     ※ 変更可能です
+    
     ※ 推奨されませんがすべて表示することも可能です
+    
 
     #### Useage
 
         >>> import jysdlib
-        
         ※ アーティストを指定する場合
         >>> jysdlib.search_song("勇者", "YOASOBI")
         ※ アーティストを指定しない場合
@@ -67,7 +75,7 @@ def search_song(song_name: str, artist: str = None, show_count: int = None, all_
         removed_sabikara_count = None
     
     header = set_data()[0]
-    get_hitcount = requests.post("https://mspxy.joysound.com/Common/ContentsList", data=set_data(song_name)[1], headers=header)
+    get_hitcount = requests.post("https://mspxy.joysound.com/Common/ContentsList", data=set_data(name=song_name)[1], headers=header)
     hitcount = get_hitcount.json()["contentsHitCount"]
     
     payload = set_data(song_name)[1]
@@ -96,3 +104,20 @@ def search_song(song_name: str, artist: str = None, show_count: int = None, all_
                     print(json.loads(get_data)["songName"])
     if include_sabikara == False:
         print("[サビカラ] を", removed_sabikara_count,"個削除しました")
+        
+def get_lyric(songid: int):
+    """
+    
+    曲の歌詞を取得します
+
+    #### Useage
+
+        >>> import jysdlib
+        >>> jysdlib.get_lyric(songid=990758)
+        ※ song_idは、省いてもﾖｼ
+    """
+    
+    header = set_data()[0]
+    res = requests.post("https://mspxy.joysound.com/Common/Lyric", data=set_data(songid=songid)[2], headers=header)
+    for lyric_data in res.json()["lyricList"]:
+        print(lyric_data["lyric"])
