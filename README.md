deff134739f57422bffbdb1008c2f9b483f3002c
NyaShinn1204 <134133778+NyaShinn1204@users.noreply.github.com>
2024-03-21 18:09:48 +0900

    First Commit
    作成中... ⚒ ⚒ ⚒

---
+++ README.md
@@ -0,0 +1,30 @@
+<p align="center">
+    <h3 align="center">joysound-lib</h3>
+    <p align="center">
+        Joysound の 歌詞取得ライブラリ<br />
+    </p>
+</p>
+
+## Coming Soon
+
+<!--
+
+## Installation
+
+**※ Python 3.10 以上のバージョンが必要です。**
+
+「joysound-lib」をインストールするには、以下のコマンドを実行します:
+
+> [!TIP]
+> 開発バージョンです! 注意をしてください
+
+```bash
+git clone https://github.com/nyashinn1204/joysound-lib.git
+
+cd joysound-lib
+
+make setup
+
+make active
+```
+--!>
---
+++ jysdlib/utils.py
@@ -0,0 +1,79 @@
+import requests
+import json
+
+def set_data(name: str = None):
+    payload1 = {
+        "kind1": "compound",
+        "word1": name,
+        "match1": "partial",
+        "kindCnt": "1",
+        "format": "group",
+        "start": "1",
+        "count": "5",
+        "sort": "popular",
+        "order": "desc",
+        "apiVer": "1.0",
+    }
+    header = {
+        "Accept":"*application/json, text/plain, */*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "ja;q=0.7",
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        "Origin": "https://www.joysound.com",
+        "Referer": "https://www.joysound.com/",
+        "Sec-Ch-Ua": "'Brave';v='123', 'Not(A:Brand';v='8', 'Chromium';v='123'",
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Platform": "'Windows'",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "Sec-Gpc": "1",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+        "X-Jsp-App-Name": "0000800"
+    }
+    return header, payload1
+
+def search_song(song_name: str, artist: str = None, show_count: int = None, all_hit: bool = False):
+    """
+    
+    曲を検索します
+
+    ※ デフォルトの表示数は5です
+    ※ 変更可能です
+    ※ 推奨されませんがすべて表示することも可能です
+
+    #### Useage
+
+        >>> import jysdlib
+        
+        ※ アーティストを指定する場合
+        >>> jysdlib.search_song("勇者", "YOASOBI")
+        ※ アーティストを指定しない場合
+        >>> jysdlib.search_song("勇者")
+        ※ 表示数を変更する場合
+        >>> jysdlib.search_song("勇者", show_count=15)
+        ※ すべて表示する場合
+        >>> jysdlib.search_song("勇者", all_hit=True)
+
+    """
+
+    header = set_data()[0]
+    get_hitcount = requests.post("https://mspxy.joysound.com/Common/ContentsList", data=set_data(song_name)[1], headers=header)
+    hitcount = get_hitcount.json()["contentsHitCount"]
+    
+    payload = set_data(song_name)[1]
+    if all_hit == True:
+        payload["count"] = hitcount        
+    elif all_hit == False and (show_count != None and show_count != 5):
+        payload["count"] = show_count
+        
+    res = requests.post("https://mspxy.joysound.com/Common/ContentsList", data=payload, headers=header)
+    for artist_data in res.json()["contentsList"]:
+        if artist != None:
+            if artist_data.get('artistName', '').lower() == 'yoasobi' and '勇者' in artist_data.get('songName', '').lower():
+                get_data = json.dumps(artist_data, indent=4, ensure_ascii=False)
+                print(json.loads(get_data)["songName"])
+            
+        else:    
+            get_data = json.dumps(artist_data, indent=4, ensure_ascii=False)
+            print(json.loads(get_data)["songName"])
